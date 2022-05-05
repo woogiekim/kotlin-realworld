d17b2e48020dbe288d8c620a9d263462e685ce14
Woogie Kim <mdir2kor@gmail.com>
2022-05-05 12:34:21 +0900

    kotlin-realworld-54 assertThatRealWorldException 일괄 적용

--- src/test/kotlin/com/woogie/realworld/domain/user/domain/UserImageTest.kt
+++ src/test/kotlin/com/woogie/realworld/domain/user/domain/UserImageTest.kt
@@ -1,7 +1,7 @@
 package com.woogie.realworld.domain.user.domain
 
-import com.woogie.realworld.exception.RealWorldException
-import org.assertj.core.api.Assertions
+import com.woogie.realworld.exception.ErrorCode.INVALID_USER_IMAGE_MAXIMUM_LENGTH
+import com.woogie.realworld.support.assertThatRealWorldException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -17,7 +17,8 @@ internal class UserImageTest {
     fun `최대길이 보다 크면 생성 실패`() {
         val image = (0 until UserImage.MAXIMUM_LENGTH + 1).joinToString("")
 
-        Assertions.assertThatExceptionOfType(RealWorldException::class.java)
-            .isThrownBy { UserImage(image) }
+        assertThatRealWorldException(INVALID_USER_IMAGE_MAXIMUM_LENGTH) {
+            UserImage(image)
+        }
     }
 }
